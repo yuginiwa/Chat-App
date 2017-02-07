75b06287dd12a872c68de8a10f732ae6cbbe0724
Carl Vincent Baydo <cvbaydo@gmail.com>
2017-02-07 10:15:59 +0800

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname, '../public');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
